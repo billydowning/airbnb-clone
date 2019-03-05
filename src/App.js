a2203f709ddc24db3fe6deafa230e8ec118e42ab
Billy Downing <wldowning@gmail.com>
2019-03-05 09:11:49 +0100

    airbnb clone with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+
+
   render() {
+    const flat = {
+          "name": "Superbe vue à 2 min du Sacré Coeur",
+          "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat5.jpg",
+          "price": 135,
+          "priceCurrency": "EUR",
+          "lat": 48.888839,
+          "lng": 2.339208
+    };
+
+    const flats = [ flat, flat, flat, flat ];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+        <div className="map">
+        </div>
       </div>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import Hello from './components/hello';
+// import * as serviceWorker from './serviceWorker';
+
+// const root = <div>
+// <Hello firstName="Billy" lastName="Downing"/>
+// <Hello firstName="Rorie" lastName="DuPrey"/>
+// </div>;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
